35fb51a0af5cf80de12d673261a1c269623a9bce
Milad Mohammadi <afraaliansite@gmail.com>
2021-09-27 00:39:05 -0700

    add font awsome

--- src/main.js
+++ src/main.js
@@ -8,6 +8,7 @@ import "./css/adminlte.css"
 import "./css/bootstrap-rtl.min.css"
 import "./css/custom-style.css"
 import "./css/persian-datepicker.min.css"
+import "./css/all.min.css"
 
 
 createApp(App).use(store).use(router).mount('#app')
